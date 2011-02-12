c76b625148306304fc2e8930555f79e207354e13
Brett g Porter <bgp@bgporter.net>
2011-02-12 14:40:43 -0500

    Added Filename and TargetPath fns
    ...with some test code.

--- mover.py
+++ mover.py
@@ -3,11 +3,21 @@
 ''' Utility to move and rename MP3 files into a master repository location.
 Requires that you have the mutagen ID parsing library installed.
 '''
+import doctest
+import mutagen
+import os
+import shutil
+import sys
 
 def Scrub(s):
    '''
       Remove any characters that we don't want in filenames or paths from a
       string.
+      >>> Scrub("No Illegal characters")
+      'No Illegal characters'
+      >>> Scrub("this: <should> /be\\ shorter?")
+      'this should be shorter'
+
    '''
    kIllegals = ":/\\?<>"
    for c in kIllegals:
@@ -21,6 +31,18 @@ def TargetPath(id3):
       @param id3 Dict containing track metadata that we use to generate the
       target path that can elsewhere be appended to the base path for our file
       move.
+      The path that we'll generate will have the forms:
+
+      Artist/Album
+      or
+      Artist/Album (disc #)
+
+      >>> d1 = {'album' : 'Low Electrical Worker', 'artist': 'Kneebody'}
+      >>> TargetPath(d1)
+      'Kneebody/Low Electrical Worker'
+      >>> d1['discnumber'] = '2/3'
+      >>> TargetPath(d1)
+      'Kneebody/Low Electrical Worker (disc 2)'
    '''
    discNumber = ""
    try:
@@ -37,8 +59,42 @@ def TargetPath(id3):
       id3["album"] = "(no album)"
 
    id3["discNumber"] = discNumber
-   return os.path.join(id3["artist"], "%(album)s%(discNumber)s" % id3)
+   return os.path.join(Scrub(id3["artist"]), Scrub("%(album)s%(discNumber)s" % id3))
+
+def Filename(id3):
+   '''
+      Constructs a filename (without extension) from the provided ID3
+      metadata.
+
+      Format: <trackNo>-<title>
+      >>> d1 = { 'tracknumber': '1', 'title' : "Teddy Ruxpin"}
+      >>> Filename(d1)
+      '01-Teddy Ruxpin'
+      >>> d1['tracknumber'] = '2/3'
+      >>> Filename(d1)
+      '02-Teddy Ruxpin'
+      >>> del d1['tracknumber']
+      >>> Filename(d1)
+      'Teddy Ruxpin'
 
 
 
+   '''
+   try:
+      trackNum = id3['tracknumber']
+      # Amazon track numbers are sometimes in the form 'x/y')
+      trackNum = trackNum.split('/')[0]
+      trackNum = "%02d-" % int(trackNum)
+   except (KeyError, ValueError):
+      trackNum = ""
+
+   return "%s%s" % (trackNum, Scrub(id3['title']))
+
+
+
+if __name__ == "__main__":
+   if len(sys.argv) >= 2 and sys.argv[1] == "-t":
+      print "TESTING..." 
+      doctest.testmod()
+      print "DONE."
 
