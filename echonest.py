5b1e7a401045e364d45fcf4fb605a7c37ca50c24
Brett g Porter <bgp@bgporter.net>
2011-02-22 08:48:38 -0500

    Adding 'echonest.py' -- uploads an MP3 file to echonest
    for automatic detection, which we attempt to put into that file's i3D3 data.

---
+++ echonest.py
@@ -0,0 +1,78 @@
+
+from mutagen.easyid3 import EasyID3
+from pprint import pprint
+import glob
+import json
+import mutagen
+import time
+import urllib
+import urllib2
+
+
+kApiKey = "DLTTSSYQQ9SL2CEIT"
+
+kBaseUrl = "http://developer.echonest.com/api/v4/track"
+
+
+def CheckFile(filePath):
+   print "Processing %s" % filePath
+   fileData = open(filePath, "rb").read()[:1024*1024]
+
+
+   parms = {   "api_key" : kApiKey,
+               "format" : "json",
+               "wait"   : "true",
+               "filetype" : "mp3",
+               "track" : fileData,
+               }
+
+
+
+   print "sending track data"
+   f = urllib2.urlopen(kBaseUrl + "/upload", urllib.urlencode(parms))
+
+   response = json.loads(f.read())
+
+   pprint(response)
+
+   if response['response']['status']['code'] == 0:
+      try:
+         f = EasyID3(filePath)
+      except mutagen.id3.ID3NoHeaderError:
+         f = mutagen.File(filePath, easy=True)
+         f.add_tags()
+
+      def GetAttribute(r, field):
+         return r['response']['track'][field]
+
+      title = GetAttribute(response, 'title')
+      artist = GetAttribute(response, 'artist')
+      album = GetAttribute(response, 'release')
+      if title or artist or album:
+         if title:
+            f["title"] = title
+         if artist:
+            f["artist"] = artist
+         if album:
+            f["album"] = album
+         f.save()
+
+
+letters = "t,u,v,w,x,y,z,num".split(',')
+
+for letter in letters:
+   print "Processing '%s' files" % letter
+   for f in glob.glob("/Volumes/homes/Music/%s/*.mp3" % letter):
+      try:
+         CheckFile(f)
+      except urllib2.HTTPError, e:
+         print str(e)
+         print "about to retry..."
+         time.sleep(4)
+         try:
+            CheckFile(f)
+         except urllib2.HTTPError, e:
+            print "Error again (%s); giving up on this file." % e
+
+
+
