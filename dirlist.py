e63f74e522a9aedbbcee4aa8d54c6db8eb839c08
Brett g Porter <bgp@bgporter.net>
2015-07-04 14:01:50 -0400

    Rewritten to use the fileSource obect.

--- dirlist.py
+++ dirlist.py
@@ -10,20 +10,50 @@ import os
 
 '''
 
-kTopDir = "/Volumes/zappa_files/music"
+# kTopDir = "/Volumes/zappa_files/music"
 
-# we ignore directories that don't split into this many parts
-kAlbumDirComponentCount = 2 + len(kTopDir.split(os.path.sep))
+# # we ignore directories that don't split into this many parts
+# kAlbumDirComponentCount = 2 + len(kTopDir.split(os.path.sep))
 
-print "looking for {0} parts".format(kAlbumDirComponentCount)
-output = []
+# print "looking for {0} parts".format(kAlbumDirComponentCount)
+# output = []
 
 
-for dirname, dirlist, filenames in os.walk(kTopDir):
-   parts = dirname.split(os.path.sep)
-   if kAlbumDirComponentCount == len(parts):
-      output.append("{0}/{1}".format(parts[-2], parts[-1]))
+# for dirname, dirlist, filenames in os.walk(kTopDir):
+#    parts = dirname.split(os.path.sep)
+#    if kAlbumDirComponentCount == len(parts):
+#       output.append("{0}/{1}".format(parts[-2], parts[-1]))
 
-for line in sorted(output):
-   print line
+# for line in sorted(output):
+#    print line
+
+import sys
+import os
+
+import fileSource
+
+if __name__ =="__main__":
+   import argparse
+   parser = argparse.ArgumentParser("List subdirectories with MP3 files.")
+   parser.add_argument("-s", "--src", action="store", nargs="?",
+      default=os.getcwd(), help="Source directory containing mp3 files")  
+
+   args = parser.parse_args()
+
+   srcPath = os.path.normpath(args.src)
+
+
+   paths = []
+
+   fs = fileSource.FileSource(srcPath)
+   for (fileType, filePath) in fs:
+      if fileSource.kDirectory == fileType:
+         relative = os.path.relpath(filePath, srcPath)
+         pathComponents = relative.split(os.sep)
+         if 2 == len(pathComponents):
+            paths.append(relative)
+
+   for p in sorted(paths, key=str.lower):
+      print p
+
 
