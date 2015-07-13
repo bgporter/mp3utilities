748a87992fbf1217ec2112baae46a55158506932
Brett g Porter <bgp@bgporter.net>
2015-07-13 09:25:55 -0400

    Rewritten recent.py
    Uses the new-style .tracks history file and the fileSource class
    to give us directories to look at. Maybe slower than the old one was.

--- recent.py
+++ recent.py
@@ -15,17 +15,28 @@
    Docs on the requirements of the MPD playlist format are at
    http://mpd.wikia.com/wiki/Playlists
 
+   2015/07/12: Rewritten to use the newer fileSource.FileSource object
+   to handle walking the directories for us and the newer trackHistory.history 
+   class that lets us track acruisition/move dates on a per-track basis instead
+   of a per-directory basis.
+
 '''
 
 import os
 import datetime
 import glob
+import time
+
+import fileSource
+import trackHistory
+from trackHistory import History
+
 
 ## per-install things:
 kMusicRoot = "/media/usb0/music/"
 kRootLen = len(kMusicRoot)
-kPLaylistDir = "/var/lib/mpd/playlists/"
-kPLaylistExt = "m3u"
+kPlaylistDir = "/var/lib/mpd/playlists/"
+kPlaylistExt = "m3u"
 
 
 class Recent(object):
@@ -84,7 +95,67 @@ class Recent(object):
             f.write("\n".join(tracks))
 
 
+def WritePlaylist(path, days, tracks):
+   fileName = "{0}-days.{1}".format(days, kPlaylistExt)
+   with open(os.path.join(path, fileName), "wt") as f:
+      tracks.sort()
+      f.write(u"\n".join(tracks).encode("utf-8"))
+
+
+def DaysAgo(startTime, days):
+   ''' Given the current time (as a datetime.datetime object) and a number
+      of days, returns the integer timestamp that many days earlier.
+   '''
+   then = startTime - datetime.timedelta(days=days)
+   return int(time.mktime(then.timetuple()))
+
+
 if __name__ == "__main__":
-   recent = Recent(kMusicRoot, [7, 30, 90, 180])
+   import argparse
+   parser = argparse.ArgumentParser("Create playlist files from recent acquisitions.")
+
+   parser.add_argument("-s", "--src", action="store", nargs="?", 
+      default=kMusicRoot, help="root directory of music collection.")
+   parser.add_argument("-d", "--dest", action="store", nargs="?",
+      default=kPlaylistDir, help="output directory for playlist files.")
+   parser.add_argument("-p", "--periods", action="store", nargs="?", 
+      default="7,30,90,180", help="list of most recent 'n' days of files to gather")
+
+   args = parser.parse_args()
+   args = vars(args)
+
+   # get the numbers from in between the commas and convert to integers.
+   periods = [int(p.strip()) for p in args['periods'].split(',')]
+   today = datetime.datetime.today()
+   cutoffs = [DaysAgo(today, days) for days in periods]
+
+   trackLists = [[] for p in periods]
+
+   srcPath = args['src']
+
+   fs = fileSource.FileSource(srcPath)
+
+   for (t, p) in fs:
+      if fileSource.kDirectory == t:
+         try: # !!! delete this after testing...
+            history = History(p)
+         except: 
+            continue
+         if history.fileExists:
+            print "dir - {0}".format(p)
+            for i, cutoff in enumerate(cutoffs):
+               tracks = history.RecentTracks(cutoff)
+               if tracks:
+                  trackLists[i].extend(tracks)
+
+   for days, trackList in zip(periods, trackLists):
+      print len(trackList)
+      WritePlaylist(args['dest'], days, trackList)
+
+
+
+
+
+   '''recent = Recent(kMusicRoot, [7, 30, 90, 180])
    recent.ScanForTracks()
-   recent.WritePlaylists()
+   recent.WritePlaylists()'''
